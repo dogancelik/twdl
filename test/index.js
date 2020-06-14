250dc1035699c879a7313833a146dc3e7e42cee9
Doğan Çelik <dogancelik93@gmail.com>
2020-06-14 14:27:54 +0300

    Add tests

---
+++ test/index.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+
+const index = require('../lib/index');
+const util = require('../lib/util');
+
+const tweets = [
+	'https://twitter.com/Minecraft/status/1258774679675904000',
+	'https://twitter.com/NVIDIAGeForce/status/1258069454312755201',
+	'https://twitter.com/IGN/status/1271196450681167875'
+];
+
+const options = {
+	avatar: false,
+	embed: false,
+	data: false,
+	overwrite: true,
+	format: util.DEFAULT_FORMAT,
+	date: false,
+	cookie: '',
+};
+
+function testIfAllDownloaded(results) {
+	for (let log of results[0]) {
+		assert.equal(log[0], 'downloaded');
+	}
+}
+
+describe('Twdl', function () {
+	this.timeout(15000);
+
+	it('should find an image and download', function () {
+		return index.downloadUrls([tweets[0]], options).then(testIfAllDownloaded);
+	});
+
+	it('should find a video and download', function () {
+		return index.downloadUrls([tweets[1]], options).then((results) => {
+			testIfAllDownloaded(results);
+			let videoUrl = results[0][0][1];
+			assert.match(videoUrl, /\.mp4/i, 'Media URL should be a mp4 video');
+			return results;
+		});
+	});
+
+	it('should find images and download', function () {
+		return index.downloadUrls([tweets[2]], options).then((results) => {
+			testIfAllDownloaded(results);
+			assert.equal(results[0].length, 4, 'Media count should be 4');
+		});
+	});
+});
+
---
+++ test/util.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+const util = require('../lib/util');
+
+const fake = {
+	tweet: 'https://twitter.com/Minecraft/status/1258774679675904000',
+	parsedMedia: {
+		original: 'https://pbs.twimg.com/media/EXgQ5hJXgAAVPEP.jpg',
+		extension: null,
+		downloadUrl: 'https://pbs.twimg.com/media/EXgQ5hJXgAAVPEP.jpg:orig',
+		basename: 'EXgQ5hJXgAAVPEP.jpg'
+	}
+};
+
+describe('Util', function () {
+	it('renderFormat should work', function (done) {
+		let format = util.renderFormat('#username#/#original#', fake.parsedMedia, fake.tweet, {});
+		assert.equal(format, 'Minecraft/EXgQ5hJXgAAVPEP.jpg', 'Rendered filename is incorrect');
+		done();
+	});
+});
