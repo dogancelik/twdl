a0001185381ea4af27c5006f9be216b5335726be
Doğan Çelik <dogancelik93@gmail.com>
2020-10-15 04:57:43 +0300

    Fix test

--- test/index.js
+++ test/index.js
@@ -10,7 +10,12 @@ const tweets = [
 	'https://twitter.com/IGN/status/1271196450681167875'
 ];
 
-const options = makeOptions();
+function makeTestOptions(downloadUrlFn) {
+	let options = makeOptions({ overwrite: true });
+	if (downloadUrlFn != null) options.downloadUrlFn = downloadUrlFn;
+	return options;
+}
+
 
 function testIfAllDownloaded(results, expected = 'downloaded') {
 	for (let log of results[0]) {
@@ -19,39 +24,34 @@ function testIfAllDownloaded(results, expected = 'downloaded') {
 }
 
 describe('Twdl', function () {
-	this.timeout(15000);
+	this.timeout(30000);
 
-	it('should find an image and download', function () {
-		return index.downloadUrls([tweets[0]], options).then(testIfAllDownloaded);
+	it('should find an image and download', async function () {
+		let results = await index.downloadUrls([tweets[0]], makeTestOptions());
+		testIfAllDownloaded(results);
 	});
 
-	it('should find a video and download', function () {
-		return index.downloadUrls([tweets[1]], options).then((results) => {
-			testIfAllDownloaded(results);
-			let videoUrl = results[0][0][1];
-			assert.match(videoUrl, /\.mp4/i, 'Media URL should be an mp4 video');
-			return results;
-		});
+	it('should find a video and download', async function () {
+		let results = await index.downloadUrls([tweets[1]], makeTestOptions());
+		testIfAllDownloaded(results);
+		let videoUrl = results[0][0][1];
+		assert.match(videoUrl, /\.mp4/i, 'Media URL should be an mp4 video');
 	});
 
-	it('should find images and download', function () {
-		return index.downloadUrls([tweets[2]], options).then((results) => {
-			testIfAllDownloaded(results);
-			assert.equal(results[0].length, 4, 'Media count should be 4');
-		});
+	it('should find images and download', async function () {
+		let results = await index.downloadUrls([tweets[2]], makeTestOptions());
+		testIfAllDownloaded(results);
+		assert.equal(results[0].length, 4, 'Media count should be 4');
 	});
 
-	it('should return with custom function', function () {
+	it('should return with custom function', async function () {
 		const notDownloaded = 'not downloaded';
 
 		function downloadUrl(mediaUrl, tweetUrl, mediaData, options) {
 			return [notDownloaded, mediaUrl];
 		}
 
-		let newOptions = makeOptions({ downloadUrlFn: downloadUrl });
-
-		return index.downloadUrls([tweets[2]], newOptions).then(results => {
-			testIfAllDownloaded(results, notDownloaded);
-		});
+		let results = await index.downloadUrls([tweets[2]], makeTestOptions(downloadUrl));
+		testIfAllDownloaded(results, notDownloaded);
 	});
 });
