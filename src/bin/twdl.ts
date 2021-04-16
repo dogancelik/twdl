cb21a62891c224d50a338f4558b9673f4f15e8e8
Doğan Çelik <dogancelik93@gmail.com>
2021-04-16 03:14:39 +0300

    Remove .js from bin name

--- src/bin/twdl.ts
+++ src/bin/twdl.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('yargs/yargs')(process.argv.slice(2))
+  .scriptName('twdl')
   .commandDir('cmds')
   .demandCommand()
   .help()
